6553b1e2094a8076bfddee023f821f2859109fbf
l0uise <louisepop14@gmail.com>
2024-09-18 13:38:42 +0200

    exercice 2: calculate

--- Exercice2/index.js
+++ Exercice2/index.js
@@ -1,18 +1,21 @@
 function calculate(number1, number2, operator) {
-    switch (operator) {
+  switch (operator) {
       case '+':
-        return number1 + number2;
+          return number1 + number2;
       case '-':
-        return number1 - number2;
+          return number1 - number2;
       case '*':
-        return number1 * number2;
+          return number1 * number2;
       case '/':
-        return number1 / number2;
+          if (number2 === 0) {
+              return 'Division by zero is not allowed';
+          }
+          return number1 / number2;
       default:
-        return 'opérateur invalide';
-    }
+          return 'Invalid operator';
   }
-  
+}
+
 // Exemples d'utilisation de la fonction
 console.log(calculate(5, 3, '+'));   // Affiche 8
 console.log(calculate(10, 4, '-'));  // Affiche 6
@@ -21,4 +24,4 @@ console.log(calculate(12, 3, '/'));  // Affiche 4
 console.log(calculate(8, 0, '/'));   // Affiche "Division by zero is not allowed"
 console.log(calculate(4, 5, '%'));   // Affiche "Invalid operator"
 
-export default calculate
+export default calculate;
